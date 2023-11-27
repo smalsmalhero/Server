9133db78ad67b813f72b0fbc3ba6ef37fb5a9e20
SmallSmallhero <152018837+smalsmalhero@users.noreply.github.com>
2023-11-27 21:48:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,14 +37,6 @@ def callback():
   except:
     print('error')
 
- 
-#訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
-
 #主程式
 import os
 if __name__ == "__main__":
